6e494ca8e0869cba15113e3f33b76deb2c79b490
Heeseop An <heeseop.an@gmail.com>
2023-07-02 14:02:39 -0700

    fix alignment

--- src/components/homepage/Project.jsx
+++ src/components/homepage/Project.jsx
@@ -9,36 +9,36 @@ const Project = (props) => {
 
     return (
         <React.Fragment>
-            <div className="project">
-                <div className="project-container">
-                    <div className="project-main">
-                        <div className="project-left-side">
-                            <div className="project-title">{title}</div>
-                            <div className="project-description">{description}</div>
-                            <div className="project-stack">{stack}</div>
-                            <Link to={link}>
-                                <div className="project-link">
-                                    <div className="project-link-icon">
-                                        <FontAwesomeIcon icon={faGithub}/>
-                                    </div>
-                                    <div className="project-link-text">{linkText}</div>
+            {/*<div className="project">*/}
+            <div className="project-container">
+                <div className="project-main">
+                    <div className="project-left-side">
+                        <div className="project-title">{title}</div>
+                        <div className="project-description">{description}</div>
+                        <div className="project-stack">{stack}</div>
+                        <Link to={link}>
+                            <div className="project-link">
+                                <div className="project-link-icon">
+                                    <FontAwesomeIcon icon={faGithub}/>
                                 </div>
-                            </Link>
-                        </div>
-                        <div className="project-right-side">
-                            <div className="project-image-container">
-                                <div className="project-image-wrapper">
-                                    <img
-                                        src={image}
-                                        alt="about"
-                                        className="project-image"
-                                    />
-                                </div>
+                                <div className="project-link-text">{linkText}</div>
+                            </div>
+                        </Link>
+                    </div>
+                    <div className="project-right-side">
+                        <div className="project-image-container">
+                            <div className="project-image-wrapper">
+                                <img
+                                    src={image}
+                                    alt="about"
+                                    className="project-image"
+                                />
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+            {/*</div>*/}
         </React.Fragment>
     );
 };
